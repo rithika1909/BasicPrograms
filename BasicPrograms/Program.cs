7ba0f42f4209926d6c128eae10067bc5e283894e
Rithika L <rithika.l@tvsnext.com>
2023-07-13 18:13:28 +0530

    [Rithika] Add : Implemented

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -7,8 +7,10 @@ namespace BasicPrograms
         {
             //FlipCoin flipcoin = new FlipCoin();
             //flipcoin.Play();
-            LeapYear leapYear = new LeapYear();
-            leapYear.CheckLeapYear();
+            //LeapYear leapYear = new LeapYear();
+            //leapYear.CheckLeapYear();
+            PowerOf2 powerOf2 = new PowerOf2();
+            powerOf2.PrintPowerOf2();
         }
 
     }
